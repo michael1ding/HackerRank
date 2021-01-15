e6d398cefe88fd25e5d06b499b7f307788174703
Michael <30552563+greenyellowblue@users.noreply.github.com>
2021-01-14 20:50:17 -0500

    a

---
+++ src/matrixTracing.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+import java.util.Scanner;
+
+
+public class matrixTracing {
+	
+	static int solve(int n, int m) {
+		return combination(n+m-2, Math.min(n, m)-1)%1000000007;
+    }
+
+    
+    private static int combination(int a, int b) {
+    	if (b == 0 || a == b) {
+    		return 1;
+    	}
+    	return factorial(a)/(factorial(b) * (factorial(a-b)));
+    }
+    
+    private static int factorial(int a) {
+    	BigInteger result = BigInteger.ONE;
+
+        for (int i = 2; i <= a; i++) {
+            result = result.multiply(BigInteger.valueOf(i%1000000007));
+        }
+
+        return result;
+    }
+
+    
+    public static void main(String[]args) {
+    	System.out.println(solve(2, 3));
+    }
+	
+	
+	
+}
